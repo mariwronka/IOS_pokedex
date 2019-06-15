f97e0008bc2b934903c37ed0848f65e70ce4afdc
Mari Wronka <marielle.wronka@hotmail.com>
2019-06-15 01:23:43 -0300

    detail

--- Pokedex/Extensions/UIProgressViewExtension.swift
+++ Pokedex/Extensions/UIProgressViewExtension.swift
@@ -14,6 +14,10 @@ extension UIProgressView {
         self.transform = self.transform.scaledBy(x: 1, y: 5)
         self.layer.cornerRadius = 10
         self.progressTintColor = color
+        self.clipsToBounds = true
+        self.layer.sublayers![1].cornerRadius = 10
+        self.layer.cornerRadius = 10
+        self.subviews[1].clipsToBounds = true
     }
 
 }
